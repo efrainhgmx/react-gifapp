862783f9c37cf50ca110a1b46c888cd9df80cc5e
Efrain Hernandez <efrainhg2050@gmail.com>
2021-06-14 11:31:42 -0500

    test on custom hook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -5,10 +5,14 @@ describe('Test on custom hook', () => {
     
     test('Should return the initial state', () => {
 
-        const resp = renderHook( () => useFetchGifs( 'One Punch') );
+        const { result } = renderHook( () => useFetchGifs( 'One Punch') );
+        const { data, loading } = result.current;
         //const { data, loading } = useFetchGifs( 'One Punch' );
 
-        console.log(resp);
+        console.log(data, loading);
+
+        expect( data ).toEqual([]);
+        expect( loading ).toBe( true );
     })
     
 })
