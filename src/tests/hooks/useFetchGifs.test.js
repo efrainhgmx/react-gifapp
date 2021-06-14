c737151c55e0ff78cd175d23cfa6ad5829d78c81
Efrain Hernandez <efrainhg2050@gmail.com>
2021-06-14 11:42:30 -0500

    Testing on custom hook using renderhook

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -3,16 +3,28 @@ import { renderHook } from '@testing-library/react-hooks';
 
 describe('Test on custom hook', () => {
     
-    test('Should return the initial state', () => {
+    test('Should return the initial state', async() => {
 
-        const { result } = renderHook( () => useFetchGifs( 'One Punch') );
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch') );
         const { data, loading } = result.current;
         //const { data, loading } = useFetchGifs( 'One Punch' );
 
         console.log(data, loading);
 
+        await waitForNextUpdate();
         expect( data ).toEqual([]);
         expect( loading ).toBe( true );
-    })
+    });
+
+    test('Should show an array of images and loading in false', async() => {
+        const { result, waitForNextUpdate } = renderHook( () => useFetchGifs( 'One Punch') );
+        await waitForNextUpdate();
+
+        const { data, loading } = result.current;
+
+        expect( data.length ).toBe( 10 );
+        expect( loading ).toBe( false );
+    });
+    
     
 })
